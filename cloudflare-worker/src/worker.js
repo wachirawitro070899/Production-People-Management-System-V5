07e6ad000edd26caf300b909844db245c67eceb4
wachirawitro070899 <wachirawit.ro070899@gmail.com>
2026-08-26 09:56:34 +0700

    Fix Google OAuth JWT grant type

--- cloudflare-worker/src/worker.js
+++ cloudflare-worker/src/worker.js
@@ -85,7 +85,7 @@ async function getGoogleAccessToken(env) {
   const response = await fetch('https://oauth2.googleapis.com/token', {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: new URLSearchParams({ grant_type: 'urn:ietf:params:oauth-grant-type:jwt-bearer', assertion })
+    body: new URLSearchParams({ grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer', assertion })
   });
   if (!response.ok) throw new Error(`OAuth ${response.status}: ${await response.text()}`);
   return (await response.json()).access_token;
